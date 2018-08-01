e6af52bc5979083ad3ce7b20050e8bdea9c09e47
tanny411 <aysha.kamal7@gmail.com>
2018-08-01 22:02:54 +0600

    white board created

---
+++ src/main/java/com/mycompany/whiteboard/Figure.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.whiteboard;
+
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ *
+ * @author Aysha Kamal
+ */
+public class Figure {
+    private JsonObject json;
+
+    public Figure(JsonObject json) {
+        this.json = json;
+    }
+
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
+
+    public JsonObject getJson() {
+        return json;
+    }
+    @Override
+    public String toString() {
+        StringWriter writer = new StringWriter();
+        Json.createWriter(writer).write(json);
+        return writer.toString();
+    }
+}
---
+++ src/main/java/com/mycompany/whiteboard/MyWhiteboard.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.whiteboard;
+
+import java.io.IOException;
+import java.util.*;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author Aysha Kamal
+ */
+@ServerEndpoint(value="/endpoint", encoders = {FigureEncoder.class}, decoders = {FigureDecoder.class})
+public class MyWhiteboard {
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+    @OnMessage
+    public void broadcastFigure(Figure figure, Session session) throws IOException, EncodeException {
+        System.out.println("broadcastFigure: " + figure);
+        for (Session peer : peers) {
+            if (!peer.equals(session)) {
+                peer.getBasicRemote().sendObject(figure);
+            }
+        }
+    }
+    @OnOpen
+    public void onOpen (Session peer) {
+        peers.add(peer);
+    }
+    @OnClose
+    public void onClose (Session peer) {
+        peers.remove(peer);
+    }
+}
